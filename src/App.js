4cbc77f50998bc08e1a72e67360415020dbb9e80
jaguars.dev <jaguarsdev.com@gmail.com>
2022-04-18 17:26:21 +0430

    layout up

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 
 import Rootlayout from './components/layout/Rootlayout'
-import Getcourses from './components/api/courses/Getcourses'
+// import Getcourses from './components/api/courses/Getcourses'
 
 function App() {
   return (
     <div className="container backdrop-blur bg-white/50 dark:bg-black/40 h-[90vh] w-[95vw] rounded-2xl overflow-y-hidden font-yekb">
       <Provider store={store}>
         <Rootlayout />
-        <Getcourses />
+        {/* <Getcourses /> */}
       </Provider>
     </div>
   );

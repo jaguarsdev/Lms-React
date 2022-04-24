f2b6b987dcf95a01e5d83bb2c91fa38dfc02a1cd
jaguars.dev <jaguarsdev.com@gmail.com>
2022-04-24 20:54:25 +0430

    add subMenu

--- src/components/layout/header/Accoundheader.js
+++ src/components/layout/header/Accoundheader.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import profile_img from '../assets/images/profile.jpg'
+import { Menu, Transition } from '@headlessui/react'
+
 import Darkandlight from './Darkandlight';
+
+import profile_img from '../assets/images/profile.jpg'
+
 
 
 const Accoundheader = () => {
@@ -8,9 +12,38 @@ const Accoundheader = () => {
         <div id="account" className="flex gap-3 items-center">
             <Darkandlight />
             <div id="login" className="md:hidden">
-                <div id="avatar" className="flex items-center">
-                    <img src={profile_img} className="w-9 h-9 rounded-full mr-3 ring-2 ring-white" alt="" />
-                </div>
+                <Menu>
+                    <div id="avatar" className="flex items-center">
+                  <Menu.Button className="w-9 h-9 rounded-full mr-3 ring-2 ring-white overflow-hidden">
+                        <img src={profile_img} className="" alt="" />
+                  </Menu.Button>
+                    </div>
+                  <Menu.Items className="flex divide-y dark:divide-gray-600 absolute flex-col left-4 mt-2 shadow-xl bg-white dark:bg-black/90 rounded-lg overflow-hidden">
+                    <Menu.Item className="py-1 px-3 hover:bg-sky-200">
+                      {({ active }) => (
+                        <a
+                          className={`${active && 'bg-blue-500'}`}
+                          href="/account-settings"
+                        >
+                          حساب کاربری
+                        </a>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item className="py-1 px-3 hover:bg-sky-200">
+                      {({ active }) => (
+                        <a
+                          className={`${active && 'bg-blue-500'}`}
+                          href="/account-settings"
+                        >
+                          تنظیمات
+                        </a>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item className="py-1 px-3 text-red-500 font-medium hover:bg-sky-200">
+                      <span className="">خروج از حساب</span>
+                    </Menu.Item>
+                  </Menu.Items>
+                </Menu>
             </div>
             <div id="logout" className="hidden md:block">
                 <a href="" className="">ثبت نام</a>

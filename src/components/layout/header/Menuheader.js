f4363597b35034d245f33a540838f52594f67aa0
jaguars.dev <jaguarsdev.com@gmail.com>
2022-05-04 07:33:15 +0430

    fixed i

--- src/components/layout/header/Menuheader.js
+++ src/components/layout/header/Menuheader.js
@@ -5,9 +5,9 @@ const Menuheader = () => {
         <div id="top-menu" className="hidden md:block">
             <nav>
                 <ul className="flex items-center gap-4">
-                    <a href="" className=""><li>صفحه نخست</li></a>
-                    <a href="" className=""><li>آوای توحید</li></a>
-                    <a href="" className=""><li>تماس با ما</li></a>
+                    <a href="https://lmsava.ir" className=""><li>صفحه نخست</li></a>
+                    <a href="https://avayetowheed.ir" className=""><li>آوای توحید</li></a>
+                    <a href="https://lmsava.ir/contact" className=""><li>تماس با ما</li></a>
                 </ul>
             </nav>
         </div>
--- src/components/layout/main/sections/slidesection/Slideside.js
+++ src/components/layout/main/sections/slidesection/Slideside.js
@@ -1,58 +1,58 @@
 import React from 'react';
 
 
-import hozori from '../../../assets/svgicon/hozori.svg';
-import majazi from '../../../assets/svgicon/majazi.svg';
-import sazman from '../../../assets/svgicon/sazman.svg';
-import beinolmelal from '../../../assets/svgicon/beinolmelal.svg';
+// import hozori from '../../../assets/svgicon/hozori.svg';
+// import majazi from '../../../assets/svgicon/majazi.svg';
+// import sazman from '../../../assets/svgicon/sazman.svg';
+// import beinolmelal from '../../../assets/svgicon/beinolmelal.svg';
 
 
 const Slideside = () => {
     return (
         <ul  id="sideSlide" className="grid grid-cols-2 gap-2 col-span-12 md:col-span-4">
 
-                                <a href=""><li className=""><div id="sideSlidBox" className="shadow-lg flex justify-between p-5 md:p-11 rounded-lg bg-gradient-to-br from-emerald-300 via-emerald-400 to-emerald-600">
+                                <a href="/"><li className=""><div id="sideSlidBox" className="shadow-lg flex justify-center items-center p-5 md:p-11 rounded-lg bg-gradient-to-br from-emerald-300 via-emerald-400 to-emerald-600">
                                     <div className="right flex justify-center flex-col md:col-span-3 inline-block">
-                                        <h3 className="text-lg md:text-2xl font-bold text-gray-900">
-            حضوری
+                                        <h3 className="text-lg md:text-2xl py-2 font-bold text-gray-900">
+            دوره ها
                                         </h3>
                                     </div>
-                                    <div className="left md:flex justify-center flex-col md:col-span-1 inline-block">
+                                    {/* <div className="left md:flex justify-center flex-col md:col-span-1 inline-block">
                                         <img src={hozori} className="w-14 h-14" alt="hozori" />
-                                    </div>
+                                    </div> */}
                                 </div></li></a>
         
-                                <a href=""><li className=""><div id="sideSlidBox" className="shadow-lg flex justify-between p-6 md:p-11 rounded-lg bg-gradient-to-br from-cyan-300 via-cyan-400 to-cyan-600">
+                                <a href="/"><li className=""><div id="sideSlidBox" className="shadow-lg flex justify-center items-center p-5 md:p-11 rounded-lg bg-gradient-to-br from-orange-300 via-orange-400 to-orange-600">
                                     <div className="right flex justify-center flex-col md:col-span-2 inline-block">
-                                        <h3 className="text-lg md:text-2xl font-bold text-gray-900">
-            مجازی
+                                        <h3 className="text-lg md:text-2xl py-2 font-bold text-gray-900">
+            تک درس
                                         </h3>
                                     </div>
-                                    <div className="left md:flex justify-center flex-col md:col-span-1 inline-block">
+                                    {/* <div className="left md:flex justify-center flex-col md:col-span-1 inline-block">
                                         <img src={majazi} className="w-14 h-14" alt="majazi" />         
-                                    </div>
+                                    </div> */}
                                 </div></li></a>
         
-                                <a href=""><li className=""><div id="sideSlidBox" className="shadow-lg flex justify-between p-5 md:p-11 rounded-lg bg-gradient-to-br from-purple-300 via-purple-400 to-purple-600">
+                                <a href="/"><li className=""><div id="sideSlidBox" className="shadow-lg flex justify-center items-center p-5 md:p-11 rounded-lg bg-gradient-to-br from-purple-300 via-purple-400 to-purple-600">
                                     <div className="right flex justify-center flex-col md:col-span-3 inline-block">
-                                        <h3 className="text-lg md:text-2xl font-bold text-gray-900">
-            بین الملل
+                                        <h3 className="text-lg md:text-2xl py-2 font-bold text-gray-900">
+            کلیپ
                                         </h3>
                                     </div>
-                                    <div className="left md:flex justify-center flex-col md:col-span-1 inline-block">
+                                    {/* <div className="left md:flex justify-center flex-col md:col-span-1 inline-block">
                                         <img src={beinolmelal} className="w-14 h-14" alt="beinolmelal" />   
-                                    </div>
+                                    </div> */}
                                 </div></li></a>
                                 
-                                <a href=""><li className=""><div id="sideSlidBox" className="shadow-lg flex justify-between p-5 md:p-11 rounded-lg bg-gradient-to-br from-yellow-300 via-yellow-400 to-yellow-600">
+                                <a href="/"><li className=""><div id="sideSlidBox" className="shadow-lg flex justify-center items-center p-5 md:p-11 rounded-lg bg-gradient-to-br from-sky-300 via-sky-400 to-sky-600">
                                     <div className="right flex justify-center flex-col md:col-span-3 inline-block">
-                                        <h3 className="text-lg md:text-2xl font-bold text-gray-900">
-            سازمان ها
+                                        <h3 className="text-lg md:text-2xl py-2 font-bold text-gray-900">
+            یک گزینه
                                         </h3>
                                     </div>
-                                    <div className="left md:flex justify-center flex-col md:col-span-1 inline-block">
+                                    {/* <div className="left md:flex justify-center flex-col md:col-span-1 inline-block">
                                         <img src={sazman} className="w-14 h-14" alt="sazman" />
-                                    </div>
+                                    </div> */}
                                 </div></li></a>
                             
                         </ul>
